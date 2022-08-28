51d880144867a8a72697df13d5a703bef94148dc
Subhajit De <subhajit.de@brainiuminfotech.com>
2022-08-28 11:39:05 +0530

    LICENSE ADDED

--- app/src/main/java/com/android/codersroutewidgets/FlexibleSwitches.java
+++ app/src/main/java/com/android/codersroutewidgets/FlexibleSwitches.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.codersroute.flexiblewidgets.FlexibleSwitch;
+
 public class FlexibleSwitches extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flexible_switches);
+        FlexibleSwitch flexibleSwitch = new FlexibleSwitch(this);
     }
 }
